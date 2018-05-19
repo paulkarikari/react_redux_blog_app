f184e771a679222bcc4ca52ca48f6fdd53bd0927
paul karikari <paulkarikari1@gmail.com>
2018-05-19 16:52:41 +0000

    make api call

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class Post extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			posts: []
+		}
+	}
+
+	componentWillMount() {
+		fetch('https://jsonplaceholder.typicode.com/posts')
+			.then(res => res.json())
+			.then(data => this.setState({ posts: data }));
+	}
+
 	render() {
 		return (
 			<div>
