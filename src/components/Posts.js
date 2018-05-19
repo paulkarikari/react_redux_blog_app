befc5ff5c6e027e0199962246565b418ff4b5062
paul karikari <paulkarikari1@gmail.com>
2018-05-19 16:56:58 +0000

    add posts to render

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -15,9 +15,16 @@ class Post extends Component {
 	}
 
 	render() {
+		const postItems = this.state.posts.map(post => (
+			<div key={post.id}>
+				<h3> {post.title} </h3>
+				<p> {post.body} </p>
+			</div>
+		))
 		return (
 			<div>
 				<h1> Post </h1>
+				{postItems}
 			</div>
 		);
 	}
